4b628de3abf1cb8446e7b20356a7c8cf3b863d80
smukhyala <smukhyala@gmail.com>
2024-02-03 17:46:22 -0800

    Revert "add changing ui per selection"
    This reverts commit 66277767a2d04957c868c832efde32101d7b2c30.

--- gui.py
+++ gui.py
@@ -47,8 +47,6 @@ def main():
     prompt = tk.Label(window, text="What would you like to cleanse?", font=ctk.CTkFont(size=15), underline = True)
     listbox = tk.Listbox(window, font=ctk.CTkFont(size=14), height = len(cleanOptions), selectmode = "multiple")
 
-    credentialsInsta = []
-
     #function to show options based on media type
     def showOptions():
         #clear exising options
@@ -56,52 +54,13 @@ def main():
 
         #get options from newly selected
         type = clicked.get()
-
-        if type == "Instagram":
-            #visual for username and password entering
-            username_label = tk.Label(window, text="Username:", font=ctk.CTkFont(size=12))
-            username_label.pack()
-            username_entry = tk.Entry(window)
-            username_entry.pack()
-            buffer = tk.Label(window, text="", font=ctk.CTkFont(size=1))
-            buffer.pack()
-            password_label = tk.Label(window, text="Password:", font=ctk.CTkFont(size=12))
-            password_label.pack()
-            password_entry = tk.Entry(window, show="*")
-            password_entry.pack()
-
-            values = []
-            username = username_entry.get()
-            password = password_entry.get()
-
-            values.append(username, password)
-            credentialsInsta = values
-
-            #visually remove entered credentials
-            username_entry.delete(0, tk.END)
-            password_entry.delete(0, tk.END)
-
-        if type == "Youtube":
-            #visual for username and password entering
-            video_label = tk.Label(window, text="Video URL:", font=ctk.CTkFont(size=12))
-            video_label.pack()
-            video_entry = tk.Entry(window)
-            video_entry.pack()
-
-            values = []
-            video = video_entry.get()
-
-            values.append(video)
-            credentialsInsta = values
-
-            #visually remove entered credentials
-            video_entry.delete(0, tk.END)
-    
         cleanOptions = []
         if type == "Instagram": 
             cleanOptions = ["Instagram Comments", "Instagram Messages"]
         elif type == "Youtube":
             cleanOptions = ["Youtube Comments"]
+        elif type == "Discord":
+            cleanOptions = ["Discord Messages"]
     
         #pack option selector and label
         prompt.pack()
@@ -118,13 +77,25 @@ def main():
     #dropdown for media type
     drop = tk.OptionMenu( window , clicked, *mediaTypes, command = grabcurrent ) 
     drop.pack(pady=10) 
-    
-    #username = username_entry.get()
-    #password = password_entry.get()
+
+    #visual for username and password entering
+    username_label = tk.Label(window, text="Username:", font=ctk.CTkFont(size=12))
+    username_label.pack()
+    username_entry = tk.Entry(window)
+    username_entry.pack()
+    buffer = tk.Label(window, text="", font=ctk.CTkFont(size=1))
+    buffer.pack()
+    password_label = tk.Label(window, text="Password:", font=ctk.CTkFont(size=12))
+    password_label.pack()
+    password_entry = tk.Entry(window, show="*")
+    password_entry.pack()
+
     #send user/pass and cleanse preferences to files
-    def submit_credentials(values):
+    def submit_credentials():
         #grab selected options dropdown and fillable fields
         text = clicked.get()
+        username = username_entry.get()
+        password = password_entry.get()
     
         #open and read credentials file
         with open("credentials.op", "r") as file:
@@ -133,18 +104,22 @@ def main():
         #add and replace corresponding username and password values
         for i, line in enumerate(lines):
             if text == "Instagram" and "Instagram:" in line:
-                username = values[0]
-                password = values[1]
+                lines[i + 1] = f"{username}\n"
+                lines[i + 2] = f"{password}\n"
+            elif text == "Discord" and "Discord:" in line:
                 lines[i + 1] = f"{username}\n"
                 lines[i + 2] = f"{password}\n"
             elif text == "Youtube" and "Youtube:" in line:
-                username, password = values[0]
                 lines[i + 1] = f"{username}\n"
                 lines[i + 2] = f"{password}\n"
         
         #write changes to file
         with open("credentials.op", "w") as file:
             file.writelines(lines)
+    
+        #visually remove entered credentials
+        username_entry.delete(0, tk.END)
+        password_entry.delete(0, tk.END)
     
         #check cleanse options checked true
         selectedVals = []
@@ -161,6 +136,8 @@ def main():
             lines[4] = f"False\n"
         if text == "Youtube":
             lines[8] = f"False\n"
+        if text == "Discord":
+            lines[12] = f"False\n"
 
         #change selected cleanse options to true
         for i in selectedVals:
@@ -170,6 +147,8 @@ def main():
                 lines[4] = f"True\n"
             elif i == "Youtube Comments":
                 lines[8] = f"True\n"
+            elif i == "Discord Messages":
+                lines[12] = f"True\n"
 
         #remove comments and messages from instagram using external script
         with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -179,8 +158,7 @@ def main():
                 future1 = executor.submit(remove, bool(rmcomments), bool(rmmessages))
                 #remove(bool(rmcomments), bool(rmmessages))
             if clicked.get() == "Youtube":
-                video_id = lines[5]
-                future1 = executor.submit(youtube.filter_comment, video_id)
+                future1 = executor.submit(youtube.filter_comment, "video_id")
 
             p = Progressbar(window,orient=HORIZONTAL,length=15000,mode="determinate",takefocus=False,maximum=500)
             p.pack()            
